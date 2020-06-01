628e0f3940a61a080cc8b609a23d28d398a5db9c
ylam004 <lamyueren@yahoo.com>
2020-06-02 00:47:49 +0800

    Updates

--- PIInterface.py
+++ PIInterface.py
@@ -1,20 +1,20 @@
 import epd7in5_V2
 import logging
 
-class PI:
-    def print(image):
-        try:
-            epd = epd7in5_V2.EPD()
-            logging.info("init")
-            self.epd.init()
-            self.epd.display(epd.getbuffer(image))
-            logging.info("Goto Sleep...")
-            self.epd.sleep()
+logging.info("init")
+epd = epd7in5_V2.EPD()
 
-        except IOError as e:
-            logging.info(e)
+def printOut(image):
+    try:        
+        epd.init()
+        epd.display(epd.getbuffer(image))
+        logging.info("Goto Sleep...")
+        epd.sleep()
 
-        except KeyboardInterrupt:
-            logging.info("ctrl + c:")
-            epd7in5_V2.epdconfig.module_exit()
-            exit()
+    except IOError as e:
+        logging.info(e)
+
+    except KeyboardInterrupt:
+        logging.info("ctrl + c:")
+        epd7in5_V2.epdconfig.module_exit()
+        exit()
